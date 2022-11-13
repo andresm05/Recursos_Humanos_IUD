4d12e38a5e9b14cc0931fa34a9f84eeb25643ff0
Jaime Andrés Muñoz <88641264+andresm05@users.noreply.github.com>
2022-11-12 21:31:58 -0500

    solución propuesta al caso de estudio

---
+++ src/dao/EstadoCivilDao.java
@@ -0,0 +1,14 @@
+
+package dao;
+
+import java.util.List;
+import model.EstadoCivil;
+
+/**
+ *
+ * @author HOME
+ */
+public interface EstadoCivilDao {
+    
+    List<EstadoCivil> findAll();
+}
---
+++ src/model/Universidad.java
@@ -0,0 +1,35 @@
+
+package model;
+
+/**
+ *
+ * @author HOME
+ */
+public class Universidad {
+
+    private int id;
+    private String nombre;
+
+    public Universidad(){
+        
+    }
+    
+    public Universidad(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
